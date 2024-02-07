02e101b3fbb1c2b8f49ce7d78161559dc9d3f506
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-02-08 00:04:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Medico</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="font-extrabold">This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
