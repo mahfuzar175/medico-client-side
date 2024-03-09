61420b2ada849664a4351a4f92a866e9c97a21ff
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-09 20:36:38 +0600

    blog customized

--- src/components/LatesPost/LatestPost.jsx
+++ src/components/LatesPost/LatestPost.jsx
@@ -1,12 +1,33 @@
-import HeadingSection from "../../Pages/Shared/HeadingSection";
+import { MdArrowOutward } from "react-icons/md";
 import { IoPersonOutline } from "react-icons/io5";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { IoBagAddOutline } from "react-icons/io5";
 
 const LatestPost = () => {
   return (
-    <div className="flex flex-col justify-center items-center font-poppins mt-4">
-      <HeadingSection title="Blog & News" description="Our Latest Posts" />
+    <div className="font-poppins mt-4">
+      <div className="mb-8 mt-[118px] lg:px-12 px-6">
+        <div className="flex md:flex-row flex-col justify-between items-center">
+          <div className="text-center md:text-start">
+            <h4 className="font-semibold text-xl text-blue-500">Our Blog</h4>
+            <h2 className="font-bold text-3xl md:text-5xl text-blue-900">
+              Our Latest Posts
+            </h2>
+          </div>
+          <div>
+          <div className="mt-8 flex flex-wrap justify-center items-center gap-4">
+                <a
+                  className="block w-full rounded-full border hover:bg-blue-900 hover:text-white border-blue-900 px-4 py-4 font-semibold text-blue-900 text-xl shadow sm:w-auto"
+                  href="#"
+                >
+                 <di className="flex justify-center items-center gap-1"> Read All Posts<MdArrowOutward /></di>
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+      {/* <HeadingSection title="Blog & News" description="Our Latest Posts" /> */}
+      <div className="flex justify-center items-center">
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:gap-14 gap-6 lg:px-12 px-6">
         <div className="space-y-5 text-blue-900 hover:text-blue-600">
           <div className="rounded-lg overflow-hidden block">
@@ -78,6 +99,7 @@ const LatestPost = () => {
             </h2>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
